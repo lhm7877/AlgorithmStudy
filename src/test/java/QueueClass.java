f8788a4c8609c38138d33075103c622982bd7bc9
lhm7877 <vkfdldh7>
2020-03-22 19:15:00 +0900

    q10828 버그 수정

---
+++ src/test/java/QueueClass.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueClass {
+    @Test
+    public void queue(){
+        Queue<Integer> queue = new PriorityQueue<>();
+        Queue<Integer> queue1 = new LinkedList<>();
+
+    }
+}
