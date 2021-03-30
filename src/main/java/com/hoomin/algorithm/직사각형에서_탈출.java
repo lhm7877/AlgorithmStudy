ecb0bf14ff88090b197e6faa1a13e4ea5a971c72
lhm7877 <vkfdldh7>
2021-03-31 00:29:03 +0900

    직사각형에서 탈출

--- src/main/java/com/hoomin/algorithm/직사각형에서_탈출.java
+++ src/main/java/com/hoomin/algorithm/직사각형에서_탈출.java
@@ -1,7 +1,16 @@
 package com.hoomin.algorithm;
 
-public class 직사각형에서_탈출 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
+public class 직사각형에서_탈출 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
+        final int[] intArray = Arrays.stream(str.split(" ")).mapToInt(Integer::parseInt).toArray();
+        System.out.println(Math.min(Math.min(intArray[2] - intArray[0], intArray[0]), Math.min(intArray[3] - intArray[1], intArray[1])));
+        reader.close();
     }
 }
