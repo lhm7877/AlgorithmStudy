7c73a47dadd2a81fe83e46369397bbe22ab4a701
이후민 <lhm7877>
2021-04-07 10:14:17 +0900

    feat: 풀이 완료

--- src/main/java/com/hoomin/algorithm/체스판_다시_칠하기.java
+++ src/main/java/com/hoomin/algorithm/체스판_다시_칠하기.java
@@ -15,28 +15,33 @@ public class 체스판_다시_칠하기 {
             System.arraycopy(row, 0, strings, 0, row.length);
         }
 
-        int result = Integer.MAX_VALUE;
+        int result1 = Integer.MAX_VALUE;
+        int result2 = Integer.MAX_VALUE;
         for (int x = 0; x < board.length - 7; x++) {
             for (int y = 0; y < board[0].length - 7; y++) {
-                int matrixCount = 0;
+                int matrixCount1 = 0;
+                int matrixCount2 = 0;
+                boolean flag = true;
                 for (int i = 0; i < 8; i++) {
                     int rowCount = 0;
                     for (int j = 0; j < 8; j++) {
                         if (answer[j].equals(board[x+i][y+j])) {
                             rowCount++;
                         }
-                        System.out.print(board[x+i][y+j]);
                     }
-                    int rowMinCount = Math.min(rowCount, 8 - rowCount);
-                    System.out.print(" " + rowMinCount);
-                    System.out.println();
-                    matrixCount += rowMinCount;
+                    if (flag) {
+                        matrixCount1 += rowCount;
+                        matrixCount2 += (8 - rowCount);
+                    } else {
+                        matrixCount1 += (8 - rowCount);
+                        matrixCount2 += rowCount;
+                    }
+                    flag = !flag;
                 }
-                result = Math.min(result, matrixCount);
-                System.out.print(" " + matrixCount);
-                System.out.println();
+                result1 = Math.min(result1, matrixCount1);
+                result2 = Math.min(result2, matrixCount2);
             }
         }
-        System.out.print(result);
+        System.out.println(Math.min(result1, result2));
     }
 }
