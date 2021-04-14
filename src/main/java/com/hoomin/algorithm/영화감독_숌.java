5bb78b7dd88dfda79a5d259c4edd11efc8ab34fe
이후민 <lhm7877>
2021-04-14 20:00:08 +0900

    feat: 영화감독 숀 다시 풀어보기

---
+++ src/main/java/com/hoomin/algorithm/영화감독_숌.java
@@ -0,0 +1,19 @@
+package com.hoomin.algorithm;
+
+import java.util.Scanner;
+
+public class 영화감독_숌 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        int count = 0;
+        int number = 0;
+        while (count < input) {
+            number++;
+            if (String.valueOf(number).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(number);
+    }
+}
