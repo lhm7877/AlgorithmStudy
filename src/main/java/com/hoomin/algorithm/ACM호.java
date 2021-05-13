278c009270fc548a57562d3066915ce2db01f56b
lhm7877 <vkfdldh7>
2021-05-14 00:41:14 +0900

    :sparkles: ACM호

---
+++ src/main/java/com/hoomin/algorithm/ACM호.java
@@ -0,0 +1,27 @@
+package com.hoomin.algorithm;
+
+import java.util.Scanner;
+
+public class ACM호 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = Integer.parseInt(scanner.nextLine());
+        while (T-- > 0) {
+            final String[] s = scanner.nextLine().split(" ");
+            Integer H = Integer.valueOf(s[0]);
+            Integer W = Integer.valueOf(s[1]);
+            Integer N = Integer.valueOf(s[2]);
+            int front;
+            if (N % H != 0) {
+                front = N % H ;
+            } else {
+                front = H;
+            }
+            int back = (N / H);
+            if (N % H != 0) {
+                back++;
+            }
+            System.out.println(front + String.format("%02d", back));
+        }
+    }
+}
