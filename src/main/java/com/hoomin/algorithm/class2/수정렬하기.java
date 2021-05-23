025eda7e5bb63b039442236a83e15da673f29c3f
lhm7877 <vkfdldh7>
2021-05-24 00:01:50 +0900

    feat: 수정렬하기

--- src/main/java/com/hoomin/algorithm/class2/수정렬하기.java
+++ src/main/java/com/hoomin/algorithm/class2/수정렬하기.java
@@ -26,4 +26,20 @@ public class 수정렬하기 {
         reader.close();
         writer.close();
     }
+
+    private static void method2() throws IOException {
+        int N;
+        int[] nums,ans;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        boolean[] arr = new boolean[2000001];
+        for (int i = 0; i <N; i++) {
+            arr[Integer.parseInt(br.readLine())+1000000] = true;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]) sb.append((i-1000000)).append('\n');
+        }
+        System.out.println(sb);
+    }
 }
